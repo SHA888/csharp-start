33cbbd09c7ec4645953ff6b23d45f480e7039c66
Kresna Sucandra <kresnasucandra@gmail.com>
2022-12-16 14:01:51 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// NET6 
+
+// Console.WriteLine("Hello, World!");
+
+// NET5
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# C# Start
+
+- NET 5.0
+- NET 6.0
+- NET 7.0
